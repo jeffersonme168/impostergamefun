8e89024e83c27f9177e7f6ef3fa721060a3f53da
jeffersonme168 <jeffersonme168@gmail.com>
2026-01-11 18:55:32 +0800

    去掉.next文件夹

--- .next/dev/server/chunks/ssr/_next-internal_server_app_game_page_actions_4770eff2.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/game/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_game_page_actions_4770eff2.js.map
